5b7cb8ef4c3e500f09767180e95b3678666e4056
Mihaela Antoci <mihaela.antoci@appliedtechnology.se>
2023-05-24 23:00:55 +0200

    Implement traffic lights using Tailwind

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,25 @@
-import "./styles/index.css";
-const main = () => {
-  console.log("Code with Ahsan, Hello World 123!");
-};
+import './styles/index.css';
 
-main();
+function trafficLights() {
+  const green = document.querySelector('#green');
+  const red = document.querySelector('#red');
+  const yellow = document.querySelector('#yellow');
+
+  setTimeout(() => {
+    green.classList.add('active');
+    yellow.classList.add('inactive');
+    red.classList.add('inactive');
+  }, 0);
+
+  setTimeout(() => {
+    yellow.classList.replace('inactive', 'active');
+    green.classList.replace('active', 'inactive');
+  }, 2000);
+
+  setTimeout(() => {
+    yellow.classList.replace('active', 'inactive');
+    red.classList.add('active');
+  }, 5000);
+}
+
+trafficLights();
